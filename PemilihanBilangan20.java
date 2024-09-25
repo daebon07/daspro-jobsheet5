42a601f42b1ba5198257792a8c5f0abb415b162c
daebon07 <muhammadshabran25@gmail.com>
2024-09-25 21:49:21 +0700

    Pemilihan bilangan

---
+++ PemilihanBilangan20.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class  PemilihanBilangan20 {
+
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        System.out.println("masukan sebuah angka");
+        int angka = Input.nextInt();
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " termasuk bilangan genap");
+        }
+        else {
+            System.out.println("amgka " + angka + " termasuk bilangan ganjil");
+        }
+        
+    }
+}
