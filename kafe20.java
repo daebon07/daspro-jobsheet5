bf3c04bcfe130d9b1c386af91737eaeed6500518
daebon07 <muhammadshabran25@gmail.com>
2024-09-26 09:43:55 +0700

    kafe 20

---
+++ kafe20.java
@@ -0,0 +1,54 @@
+import java . util .Scanner;
+public class kafe20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        String menu ;
+        char ukurancup;
+        int jumlah;
+        boolean keanggotaan;
+
+        System.out.println("masukan menu : ");
+        menu = input.nextLine();
+        System.out.println("masukan ukuran cup : ");
+        ukurancup = input.next().charAt(0);
+        System.out.println("masukan jumlah : ");
+        jumlah = input.nextInt();
+        System.out.println("masukan keanggotaan (true/false) :  ");
+        keanggotaan = input.nextBoolean();
+
+        double hargamenu= 0;
+        switch (menu.toLowerCase()) {
+            case "kopi":
+            hargamenu = 12000;
+            break;
+            case "teh":
+            hargamenu = 7000;
+            break;
+            case "coklat":
+            hargamenu = 20000;
+            break;
+        }
+
+        double totalharga = hargamenu*jumlah;
+        
+        switch (ukurancup) {
+            case 'S':
+            break;
+            case 'M':
+            totalharga += 0.25 * totalharga;
+            break; 
+            case  'L':
+            totalharga += 0.4 * totalharga;
+            break;
+        
+        }
+
+        double diskon = keanggotaan ? 0 : 0.1;
+        double nominalbayar = totalharga - (diskon * totalharga);
+
+        System.out.println( "item pembelian : " + jumlah + " " + menu + "dengan ukuran cup " + ukurancup);
+        System.out.println("nominal bayar " + nominalbayar);
+
+    }
+}
